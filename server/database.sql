16f0b35c7687c6b413832f8521ce5d9d6662025c
Freddie <f.groom@dectech.co.uk>
2020-10-22 15:51:17 +0100

    init commit

---
+++ server/database.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE mobilesurvey
+
+CREATE TABLE benefits(
+  benefit_id serial PRIMARY KEY,
+  mainString text,
+  shortString text,
+  hoverString text,
+  string text,
+  icon text,
+  img text
+)
